ed32d8e91ccea5ac70a14fbbcf74f08684712337
Jiangc <jiangc@vistring.com>
2022-12-07 10:16:54 +0800

    feat: rust-desktop plugin

---
+++ buildSrc/src/main/kotlin/CargoDesktopPlugin.kt
@@ -0,0 +1,28 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.register
+
+/**
+ * TODO
+ * - 支持增量构建，如果 Rust 工程下源代码没有发生变化的话，避免重新编译
+ */
+class CargoDesktopPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.extensions.create(EXTENSION_NAME, CargoDesktopExtension::class.java)
+        // 这里必须是 afterEvaluate，不然读取不到对应的配置值
+        target.afterEvaluate {
+            target.tasks.maybeCreate(TASK_NAME, CargoBuildTask::class.java).apply {
+                group = TASK_GROUP
+                description = "Build rust libray"
+            }
+        }
+    }
+
+    companion object {
+        const val EXTENSION_NAME = "cargo"
+
+        const val TASK_GROUP = "rust"
+        const val TASK_NAME = "cargoBuild"
+    }
+}
