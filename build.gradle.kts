852e6b32cdac50cadf490dddbbedac489b1531a0
Jiangc <jiangc@vistring.com>
2023-05-29 11:40:06 +0800

    chore: update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,20 @@
 plugins {
-    `kotlin-dsl`
+    id("java-gradle-plugin")
+    id("maven-publish")
 }
 
-dependencies {
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+
+dependencies {
+    implementation(gradleKotlinDsl())
+}
+
+group = "com.github.foodiestudio.plugin"
+version = "0.0.1-SNAPSHOT"
 
 // 为 buildSrc 里的 Plugin 创建对应的 id，这样才能在 plugins{} 中使用
 gradlePlugin {
