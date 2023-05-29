ffa78f3ee92d2b16567f8ad9480952d2f0d7d5ad
Jiangc <jiangc@vistring.com>
2023-05-29 11:44:15 +0800

    feat: add uniffi task

--- src/main/kotlin/CargoDesktopPlugin.kt
+++ src/main/kotlin/CargoDesktopPlugin.kt
@@ -1,19 +1,20 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.register
 
-/**
- * TODO
- * - 支持增量构建，如果 Rust 工程下源代码没有发生变化的话，避免重新编译
- */
 class CargoDesktopPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         target.extensions.create(EXTENSION_NAME, CargoDesktopExtension::class.java)
         // 这里必须是 afterEvaluate，不然读取不到对应的配置值
         target.afterEvaluate {
-            target.tasks.maybeCreate(TASK_NAME, CargoBuildTask::class.java).apply {
+            target.tasks.maybeCreate(TASK_CARGO_BUILD_NAME, CargoBuildTask::class.java).apply {
                 group = TASK_GROUP
                 description = "Build rust libray"
+            }
+            target.tasks.maybeCreate(TASK_UNFFI_BINDING_NAME, UnffiBindingTask::class.java).apply {
+                group = TASK_GROUP
+                description = "Generate UnffiBinding class"
             }
         }
     }
@@ -22,6 +23,7 @@ class CargoDesktopPlugin : Plugin<Project> {
         const val EXTENSION_NAME = "cargo"
 
         const val TASK_GROUP = "rust"
-        const val TASK_NAME = "cargoBuild"
+        const val TASK_CARGO_BUILD_NAME = "cargoBuild"
+        const val TASK_UNFFI_BINDING_NAME = "uniffiBuild"
     }
 }
